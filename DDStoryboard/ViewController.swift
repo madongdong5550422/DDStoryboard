3f0e96441590b939b8d8e358256947a71b384575
madongdong <290555600@qq.com>
2017-04-18 23:26:54 +0800

    添加录制的功能代码

--- DDStoryboard/ViewController.swift
+++ DDStoryboard/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 import SpriteKit
 import GameplayKit
 import SnapKit
+import ReplayKit
 
 class ViewController: UIViewController {
+    
+    var isRecording = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +39,39 @@ class ViewController: UIViewController {
         
         // Present the scene.
         gameView.presentScene(scene)
+        
+        let recordButton = UIButton();
+        self.view.addSubview(recordButton)
+        recordButton.backgroundColor = UIColor.red
+        recordButton.snp.makeConstraints { (make) in
+            make.width.equalTo(60)
+            make.height.equalTo(40)
+            make.bottom.equalTo(self.view)
+        }
+        recordButton.addTarget(self, action: #selector(recordButtonClick(sender:)), for: UIControlEvents.touchUpInside)
+        
+    }
+    
+    func recordButtonClick(sender:Any) {
+        print("录制按钮点击")
+        
+        if isRecording {
+            
+            RPScreenRecorder.shared().stopRecording { (preViewController, nil) in
+                self.isRecording = false
+                print("结束成功")
+                self.present(preViewController!, animated: true, completion: { 
+                    
+                });
+            }
+        } else {
+            
+            RPScreenRecorder.shared().startRecording { (nil) in
+                self.isRecording = true
+                print("开始成功")
+            }
+        }
+        
         
     }
 
